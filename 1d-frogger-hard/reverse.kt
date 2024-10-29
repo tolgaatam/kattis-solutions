46412e94204d5d20b679dc541b5a1f4ce90c5d73
tolgaatam <atam@sabanciuniv.edu>
2024-10-29 16:13:58 +0300

    1d-frogger-hard: improve reverse solution

--- 1d-frogger-hard/reverse.kt
+++ 1d-frogger-hard/reverse.kt
@@ -1,47 +1,59 @@
-import java.util.Arrays.fill
+import java.util.LinkedList
 
 fun main() {
     val n = readln().toInt()
     val board = readln().split(" ").map { it.toInt() }
 
-    val possibleMagicNumbers = board.toSet() // creates a LinkedHashSet, which is faster for readonly sets like this
-    val memo = arrayOfNulls<Boolean>(n)
+    val possibleMagicNumbers = HashMap<Int, MutableList<Int>>()
+    val reverseGraph = Array<MutableList<Int>>(n){LinkedList()} // shows incoming edges
+
+    @OptIn(ExperimentalStdlibApi::class)
+    for (s in 0 ..< n){
+        val sVal = board[s]
+        if(!possibleMagicNumbers.containsKey(sVal)){
+            possibleMagicNumbers[sVal] = ArrayList()
+        }
+        possibleMagicNumbers[sVal]!!.add(s)
+
+        val sNext = s + board[s]
+        @OptIn(ExperimentalStdlibApi::class)
+        if(sNext in 0..<n){
+            reverseGraph[sNext].add(s)
+        }
+    }
 
     var numberOfWinningOutcomes = 0
 
-    for (magicNumber in possibleMagicNumbers){
-        fill(memo, null)
+    for (magicNumber in possibleMagicNumbers.keys){
+        val winningIndices = HashSet<Int>()
 
-        var discoveredThisRound = 0
-        // Detect direct wins
+        val discoveredIndicesThisRound = HashSet<Int>()
+        val discoveredIndicesLastRound = HashSet<Int>()
+
+        // Detect starting indices which yield the magic number directly
         @OptIn(ExperimentalStdlibApi::class)
-        for(s in 0 ..< n){
-            if(board[s] == magicNumber){
-                memo[s] = true
-                discoveredThisRound++
-            }
+        for(s in possibleMagicNumbers[magicNumber]!!){
+            winningIndices.add(s)
+            discoveredIndicesThisRound.add(s)
         }
-        numberOfWinningOutcomes += discoveredThisRound
 
-        while (discoveredThisRound > 0){
-            discoveredThisRound = 0
+        while (discoveredIndicesThisRound.size > 0){
+            discoveredIndicesLastRound.clear()
+            discoveredIndicesLastRound.addAll(discoveredIndicesThisRound)
+            discoveredIndicesThisRound.clear()
+
             @OptIn(ExperimentalStdlibApi::class)
-            for(currIndex in 0 ..< n){
-                if(memo[currIndex] != null) {
-                    continue
-                }
-
-                val nextIndex = currIndex + board[currIndex]
-                if(nextIndex < 0 || nextIndex >= n || memo[nextIndex] == false){
-                    memo[currIndex] = false
-                } else if(memo[nextIndex] == true){
-                    memo[currIndex] = true
-                    discoveredThisRound++
+            for(s in discoveredIndicesLastRound){
+                for(incomingS in reverseGraph[s]){
+                    if(incomingS !in winningIndices){
+                        winningIndices.add(incomingS)
+                        discoveredIndicesThisRound.add(incomingS)
+                    }
                 }
             }
-
-            numberOfWinningOutcomes += discoveredThisRound
         }
+
+        numberOfWinningOutcomes += winningIndices.size
     }
 
     println(numberOfWinningOutcomes)
