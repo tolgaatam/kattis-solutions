96400013b9e5849d336595298647c5e7e445404c
tolgaatam <atam@sabanciuniv.edu>
2024-11-01 14:01:39 +0300

    10-kinds-of-people: implemented ideas for improvement

--- 10-kinds-of-people/README.md
+++ 10-kinds-of-people/README.md
@@ -1,7 +1,9 @@
-This solution solves the problem in 0.02 seconds. As there are a lot of 0.01second solutions, I do not show up in the leaderboard yet. Target is 0.01 seconds!
+This solution solves the problem in 0.02 seconds. As there are a lot of 0.01second solutions, I do not show up in the leaderboard yet.
 
-Furher improvement ideas:
+Further improvement ideas:
 
 - Put a fence of width-1 around the board to make the inner part of BFS leaner.
-- Store pair<int, int> in the queue rather than a compount integer
+- Store pair<int, int> in the queue rather than a compound integer
 - Access matrices directly like `matrix[r][c]` , rather than `matrix[ACC(r,c)]`
+
+NOTE: Applied all the techniques above, still at 0.02 seconds. Giving up :)
--- 10-kinds-of-people/main.cpp
+++ 10-kinds-of-people/main.cpp
@@ -2,63 +2,84 @@
 #include <cstring>
 #include <vector>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
-#define ACC(rr,cc) ((rr) * (cols) + (cc))
+#define BOUNDARY USHRT_MAX
+#define BOUNDARY_GROUP UINT_MAX
+#define GROUP_NOT_SET 0
+
+#define BINARY 0
+#define DECIMAL 1
+
+#define ACC(rr,cc) ((rr) * ((cols)+2) + (cc))
 #define SEW(rr,cc) (((rr) << 10) | (cc))
 #define ROW_OF_SEWED(s) ((s) >> 10)
 #define COL_OF_SEWED(s) ((s) & 0x3ff)
+
 
 int main() {
     int rows, cols;
     cin >> rows >> cols;
 
-    unsigned char matrix[rows * cols];
-    unsigned int matrixGroups[rows * cols];
-    memset(matrix, 0, rows * cols * sizeof(unsigned char));
-    memset(matrixGroups, 0, rows * cols * sizeof(unsigned int));
+    unsigned char matrix[(rows+2) * (cols+2)];
+    unsigned int matrixGroups[(rows+2) * (cols+2)];
+    memset(matrix, 0, (rows+2) * (cols+2) * sizeof(unsigned char));
+    memset(matrixGroups, 0, (rows+2) * (cols+2) * sizeof(unsigned int));
 
-    int matrixIndex = 0;
-    for (int i = 0; i < rows; ++i) {
+    for (int i = 1; i <= rows; ++i) {
         getchar_unlocked();
-        for (int j = 0; j < cols; ++j) {
-            matrix[matrixIndex++] = getchar_unlocked() - 48;
+        for (int j = 1; j <= cols; ++j) {
+            matrix[ACC(i, j)] = getchar_unlocked() - 48;
         }
     }
     getchar_unlocked();
 
+    for(int j = 0; j <= cols+1; j++) {
+        matrix[ACC(0, j)] = BOUNDARY;
+        matrix[ACC(rows+1, j)] = BOUNDARY;
+        matrixGroups[ACC(0, j)] = BOUNDARY_GROUP;
+        matrixGroups[ACC(rows+1, j)] = BOUNDARY_GROUP;
+    }
+    for(int i = 0; i <= rows+1; i++) {
+        matrix[ACC(i, 0)] = BOUNDARY;
+        matrix[ACC(i, cols+1)] = BOUNDARY;
+        matrixGroups[ACC(i, 0)] = BOUNDARY_GROUP;
+        matrixGroups[ACC(i, cols+1)] = BOUNDARY_GROUP;
+    }
+
     // process the matrix and find out connected components
     queue<int> q;
     unsigned int currGroupId = 0;
-    for(int startR = 0; startR < rows; ++startR) {
-        for(int startC = 0; startC < cols; ++startC) {
-            if(matrixGroups[ACC(startR,startC)] != 0) {
+    for(int startR = 1; startR <= rows; ++startR) {
+        for(int startC = 1; startC <= cols; ++startC) {
+            if(matrixGroups[ACC(startR, startC)] != GROUP_NOT_SET) {
                 continue;
             }
 
             matrixGroups[ACC(startR, startC)] = ++currGroupId;
-            unsigned char currGroupPersonType = matrix[ACC(startR,startC)];
+            const unsigned char currGroupPersonType = matrix[ACC(startR, startC)];
             q.push(SEW(startR, startC));
             while (!q.empty()) {
-                int sewed = q.front();
+                const int sewed = q.front();
                 q.pop();
                 const int r = ROW_OF_SEWED(sewed);
                 const int c = COL_OF_SEWED(sewed);
 
-                if(r > 0 && matrixGroups[ACC(r-1, c)] == 0 && matrix[ACC(r-1, c)] == currGroupPersonType) {
+                if(matrixGroups[ACC(r-1, c)] == GROUP_NOT_SET && matrix[ACC(r-1, c)] == currGroupPersonType) {
                     matrixGroups[ACC(r-1, c)] = currGroupId;
                     q.push(SEW(r-1, c));
                 }
-                if(r < rows-1 && matrixGroups[ACC(r+1, c)] == 0 && matrix[ACC(r+1, c)] == currGroupPersonType) {
+                if(matrixGroups[ACC(r+1, c)] == GROUP_NOT_SET && matrix[ACC(r+1, c)] == currGroupPersonType) {
                     matrixGroups[ACC(r+1, c)] = currGroupId;
                     q.push(SEW(r+1, c));
                 }
-                if(c > 0 && matrixGroups[ACC(r, c-1)] == 0 && matrix[ACC(r, c-1)] == currGroupPersonType) {
+                if(matrixGroups[ACC(r, c-1)] == GROUP_NOT_SET && matrix[ACC(r, c-1)] == currGroupPersonType) {
                     matrixGroups[ACC(r, c-1)] = currGroupId;
                     q.push(SEW(r, c-1));
                 }
-                if(c < cols-1 && matrixGroups[ACC(r, c+1)] == 0 && matrix[ACC(r, c+1)] == currGroupPersonType) {
+                if(matrixGroups[ACC(r, c+1)] == GROUP_NOT_SET && matrix[ACC(r, c+1)] == currGroupPersonType) {
                     matrixGroups[ACC(r, c+1)] = currGroupId;
                     q.push(SEW(r, c+1));
                 }
@@ -72,10 +93,9 @@ int main() {
     int row1, col1, row2, col2;
     for (int i = 0; i < numberOfQueries; ++i) {
         cin >> row1 >> col1 >> row2 >> col2;
-        row1--; col1--; row2--; col2--;
 
         if(matrixGroups[ACC(row1, col1)] == matrixGroups[ACC(row2, col2)]) {
-            if(matrix[ACC(row1, col1)] == 0) {
+            if(matrix[ACC(row1, col1)] == BINARY) {
                 cout << "binary" << endl;
             } else {
                 cout << "decimal" << endl;
