0bcc464256d953b9ee033c48fec543bfd7d715c9
tolgaatam <atam@sabanciuniv.edu>
2024-11-01 05:18:12 +0300

    distinctive-character: achieved the best

--- distinctive-character/README.md
+++ distinctive-character/README.md
@@ -1,3 +1,3 @@
-There are several iterations to the problem. DFS based solutions `dfs_naive.cpp` and `dfs_advanced.cpp` suffered in terms of performance as cases grow.
+There are several iterations to the problem. DFS based solutions `dfs_naive.cpp` and `dfs_advanced.cpp` suffered in terms of performance as cases grew.
 
-`bfs.cpp` looks good for now, but it could not pass all tests yet.
+`bfs.cpp` achieved 0.04seconds, which is the fastest runtime for this problem among all languages (by the time of writing this README).
--- distinctive-character/bfs.cpp
+++ distinctive-character/bfs.cpp
@@ -1,66 +1,48 @@
 #include <iostream>
 #include <cstring>
-#include <string>
 #include <vector>
 #include <climits>
 #include <queue>
 
 using namespace std;
 
-#define MASK(num) (1 << num)
-
-// A fast queue implementation fit for my purposes
-class MyQueue {
-public:
-    explicit MyQueue(const unsigned int maxEnqueueSize) {
-        container = new unsigned int[maxEnqueueSize];
-    }
-    ~MyQueue() {
-        delete[] container;
-    }
-    void enqueue(const unsigned int character) {
-        container[firstEmptyIndex++] = character;
-    }
-    unsigned int dequeue() { // possibly crashing behaviour, shouldn't be called without checking isEmpty()
-        return container[nextIndexToFetch++];
-    }
-    bool isEmpty() const {
-        return nextIndexToFetch == firstEmptyIndex;
-    }
-private:
-    unsigned int* container;
-    unsigned int nextIndexToFetch = 0;
-    unsigned int firstEmptyIndex = 0;
-};
+#define MASK(num) (1 << (num))
 
 int main() {
     unsigned int n,k;
     cin >> n >> k;
     const int maxNodes = 1 << k;
     unsigned int otherCharacters[n];
-    string temp;
     for(unsigned int i = 0; i < n; i++) {
-        cin >> temp;
-        otherCharacters[i] = stoi(temp, nullptr, 2);
-    }
+        unsigned int val = 0;
+        getchar_unlocked();
+        for(unsigned int m = 1 << k-1; m > 0; m >>= 1) {
+            val += (getchar_unlocked() - '0') * m;
+        }
 
-    auto q = MyQueue(maxNodes);
+        otherCharacters[i] = val;
+    }
+    getchar_unlocked();
+
+    queue<unsigned int> q;
     bool visited[maxNodes];
-    //memset(&visited, 0, sizeof(unsigned char) * maxNodes);
+    memset(visited, 0, sizeof(visited));
     for(unsigned int i = 0; i < n; i++) {
         visited[otherCharacters[i]] = true;
-        q.enqueue(otherCharacters[i]);
+        q.push(otherCharacters[i]);
     }
-    unsigned int globalBestCharacter = otherCharacters[n-1];
 
-    while(!q.isEmpty()) {
-        const unsigned int ch = q.dequeue();
+    unsigned int globalBestCharacter;
+
+    while(!q.empty()) {
+        const unsigned int ch = q.front();
+        q.pop();
 
         for(unsigned int i = 1; i < maxNodes; i <<= 1) {
             const auto nextCh = ch ^ i;
             if(!visited[nextCh]) {
                 visited[nextCh] = true;
-                q.enqueue(nextCh);
+                q.push(nextCh);
                 globalBestCharacter = nextCh;
             }
         }
