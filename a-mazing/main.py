dbb2548e59a9f11d9bb211d671eaeeb4113b542b
tolgaatam <atam@sabanciuniv.edu>
2024-11-06 15:20:34 +0300

    a-mazing: fix movement after direction order change

--- a-mazing/main.py
+++ a-mazing/main.py
@@ -21,13 +21,13 @@ maze = [[Cell(i, j) for j in range(203)] for i in range(203)]
 def find_destination(cell, next_move):
     match next_move:
         case 0:  # up
-            return maze[cell.row - 1][cell.column]
-        case 1:  # right
-            return maze[cell.row][cell.column + 1]
-        case 2:  # down
             return maze[cell.row + 1][cell.column]
-        case _:  # left
+        case 1:  # left
             return maze[cell.row][cell.column - 1]
+        case 2:  # down
+            return maze[cell.row - 1][cell.column]
+        case _:  # right
+            return maze[cell.row][cell.column + 1]
 
 
 def find_move_anti(given_move):
@@ -45,7 +45,8 @@ def main():
         incoming_move = stack_object.incoming_move
         curr_cell.visited = True
         move_chosen = None
-        for move in filter(lambda m: (not find_destination(curr_cell, m).visited) and (not curr_cell.walls[m]), range(4)):
+        for move in filter(lambda m: (not find_destination(curr_cell, m).visited) and (not curr_cell.walls[m]),
+                           range(4)):
             stdout.write(move_names[move] + '\n')
             stdout.flush()
             response = input()
