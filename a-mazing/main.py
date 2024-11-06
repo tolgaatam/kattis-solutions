b17b4eb8a596c8170f1e63c50c0686bce055f30a
tolgaatam <atam@sabanciuniv.edu>
2024-11-06 14:54:52 +0300

    a-mazing: small performance improvement

--- a-mazing/main.py
+++ a-mazing/main.py
@@ -1,13 +1,11 @@
 from sys import stdout, exit
 
-move_names = ["up", "right", "down", "left"]
-
 
 class Cell:
     def __init__(self, r, c):
         self.row = r
         self.column = c
-        self.walls = [False] * 4  # True -> wall or known to not solve
+        self.walls = [False] * 4  # True -> wall
         self.visited = False
 
 
@@ -36,47 +34,55 @@ def find_move_anti(given_move):
     return (given_move + 2) % 4
 
 
-stack = [StackElement(maze[101][101], None)]
-while True:
-    stack_object = stack[-1]
-    curr_cell = stack_object.cell
-    curr_cell.visited = True
-    move_chosen = None
-    for move in filter(lambda m: (not curr_cell.walls[m]) and (not find_destination(curr_cell, m).visited), range(4)):
-        stdout.write(move_names[move] + '\n')
-        stdout.flush()
-        response = input()
-        match response:
-            case "solved":
-                exit(0)
-            case "wall":
-                curr_cell.walls[move] = True
-            case "ok":
-                move_chosen = move
-                break
-            case _:  # wrong
-                exit(1)
+move_names = ["up", "right", "down", "left"]
 
-    if move_chosen is not None:
-        stack.append(StackElement(find_destination(curr_cell, move_chosen), move_chosen))
-    else:
+
+def main():
+    stack = [StackElement(maze[101][101], None)]
+    while True:
+        stack_object = stack[-1]
+        curr_cell = stack_object.cell
         incoming_move = stack_object.incoming_move
-        if incoming_move is None:
-            stdout.write("no way out\n")
+        curr_cell.visited = True
+        move_chosen = None
+        for move in filter(lambda m: (not find_destination(curr_cell, m).visited) and (not curr_cell.walls[m]), range(4)):
+            stdout.write(move_names[move] + '\n')
             stdout.flush()
             response = input()
-            if response == "solved":
-                exit(0)
-            else:
-                exit(1)
+            match response:
+                case "solved":
+                    exit(0)
+                case "wall":
+                    curr_cell.walls[move] = True
+                case "ok":
+                    move_chosen = move
+                    break
+                case _:  # wrong
+                    exit(1)
 
-        stdout.write(move_names[find_move_anti(incoming_move)] + '\n')
-        stdout.flush()
-        response = input()
-        match response:
-            case "ok":
-                stack.pop()
-            case "solved":  # not expected
-                exit(0)
-            case _:  #
-                exit(1)
+        if move_chosen is not None:
+            stack.append(StackElement(find_destination(curr_cell, move_chosen), move_chosen))
+        else:
+            if incoming_move is None:
+                stdout.write("no way out\n")
+                stdout.flush()
+                response = input()
+                if response == "solved":
+                    exit(0)
+                else:
+                    exit(1)
+
+            stdout.write(move_names[find_move_anti(incoming_move)] + '\n')
+            stdout.flush()
+            response = input()
+            match response:
+                case "ok":
+                    stack.pop()
+                case "solved":  # not expected
+                    exit(0)
+                case _:  #
+                    exit(1)
+
+
+if __name__ == "__main__":
+    main()
