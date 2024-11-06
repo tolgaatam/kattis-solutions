a6337ea80688a2f8655f3d27b4d328d6f73e3414
tolgaatam <atam@sabanciuniv.edu>
2024-11-07 02:10:48 +0300

    a-mazing: make movements an IntEnum for code clarity

--- a-mazing/README.md
+++ a-mazing/README.md
@@ -1,3 +1,3 @@
 I wanted to solve this in Python to practice on Python classes and so on.
 
-Solution is accepted now but the performance is not top-notch. I might tune it a bit in the future. 0.35 seconds for now.
+Solution is accepted now but the performance is not top-notch: 0.37 seconds.
--- a-mazing/main.py
+++ a-mazing/main.py
@@ -1,4 +1,15 @@
-from sys import stdout, exit
+from sys import exit
+from enum import IntEnum
+
+
+class Move(IntEnum):
+    UP = 0
+    LEFT = 1
+    DOWN = 2
+    RIGHT = 3
+
+    def __str__(self):
+        return self.name.lower()
 
 
 class Cell:
@@ -13,37 +24,34 @@ maze = [[Cell(i, j) for j in range(203)] for i in range(203)]
 
 def find_destination(cell, next_move):
     match next_move:
-        case 0:  # up
+        case Move.UP:
             return maze[cell.row + 1][cell.column]
-        case 1:  # left
+        case Move.LEFT:
             return maze[cell.row][cell.column - 1]
-        case 2:  # down
+        case Move.DOWN:
             return maze[cell.row - 1][cell.column]
         case _:  # right
             return maze[cell.row][cell.column + 1]
 
 
 def find_move_anti(given_move):
-    return (given_move + 2) % 4
-
-
-move_names = ["down", "left", "up", "right"]
+    return Move((given_move.value + 2) % 4)
 
 
 def dfs_base(curr_cell):
     curr_cell.visited = True
 
-    for move in range(4):
+    for move in Move:
         dest_cell = find_destination(curr_cell, move)
         if dest_cell.visited:
             continue
-        print(move_names[move])
-        match input():
-            case "solved":
+        print(move)
+        match input()[0]:
+            case "s":  # solved
                 exit(0)
-            case "wall":
+            case "w":  # wall
                 continue
-            case "ok":
+            case "o":  # ok
                 dfs(dest_cell, move)
             case _:  # wrong
                 exit(1)
@@ -53,24 +61,24 @@ def dfs(curr_cell, incoming_move):
     curr_cell.visited = True
     reverse_incoming_move = find_move_anti(incoming_move)
 
-    for move in range(4):
+    for move in Move:
         if move == reverse_incoming_move:
             continue
         dest_cell = find_destination(curr_cell, move)
         if dest_cell.visited:
             continue
-        print(move_names[move])
-        match input():
-            case "solved":
+        print(move)
+        match input()[0]:
+            case "s":  # solved
                 exit(0)
-            case "wall":
+            case "w":  # wall
                 continue
-            case "ok":
+            case "o":  # ok
                 dfs(dest_cell, move)
             case _:  # wrong
                 exit(1)
 
-    print(move_names[reverse_incoming_move])
+    print(reverse_incoming_move)
     input()  # input must be "ok" as I'm returning to a previous cell
 
 
@@ -78,8 +86,7 @@ def main():
     dfs_base(maze[101][101])
 
     print("no way out")
-    response = input()
-    if response == "solved":
+    if input() == "solved":
         exit(0)
     else:
         exit(1)
