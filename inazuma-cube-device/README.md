c08be8b001fa706562813260931b4394507ba095
tolgaatam <atam@sabanciuniv.edu>
2025-11-24 04:29:33 +0300

    inazuma-cube-device: improve performance even further

--- inazuma-cube-device/README.md
+++ inazuma-cube-device/README.md
@@ -1,3 +1,3 @@
 This problem came out to be a lot easier than I thought it would be. Some constraints make the problem a lot easier than first anticipated. There are some tricky things in the problem definition, too.
 
-My typescript implementation passed many c++ solutions and solutions in other languages in speed, and became 2nd fastest overall. I'm happy with the result.
+My Typescript implementation passed all C++ solutions and solutions in other languages in speed, and became 1st overall. Being able to achieve this result in a high-level language like Typescript is quite satisfying.
--- inazuma-cube-device/src/solution.ts
+++ inazuma-cube-device/src/solution.ts
@@ -26,22 +26,28 @@ function solve() {
     const m = readInt();
     const k = readInt();
 
-    const initialStates: number[] = new Array(n + 1).fill(0);
+    const states = new Int32Array(n + 1).fill(0);
     // We'll use 1-based indexing for convenience to match the problem description, making the index-0 a dummy index.
     for (let c = 1; c <= n; c++) {
-        initialStates[c] = readInt();
+        states[c] = readInt();
     }
 
-    const adj: number[][] = Array.from({ length: n + 1 }, () => []);
+    // head[u] stores the index of the first edge for node u
+    // next[i] stores the index of the next edge in the list
+    // to[i] stores the destination node for edge i
+    const head = new Int32Array(n + 1).fill(-1);
+    const next = new Int32Array(m);
+    const to = new Int32Array(m);
 
+    // Build the graph
     for (let i = 0; i < m; i++) {
         const u = readInt();
-        const v = readInt();
-        adj[u].push(v);
-    }
 
-    // Array to track incoming strikes from chain reactions
-    const incoming: number[] = new Array(n + 1).fill(0);
+        // Add edge u -> v at index i
+        to[i] = readInt();
+        next[i] = head[u];
+        head[u] = i;
+    }
 
     let totalStrikes = 0;
 
@@ -50,15 +56,17 @@ function solve() {
     // However, since relations are promised to be between a->b such that a < b, we can simply iterate from 1 to n without breaking the topological order in the DAG.
     // Also, the problem states that we should print "-1" if it's impossible, but given the constraints, it's always possible to achieve the goal as there are no cycles.
     for (let c = 1; c <= n; c++) {
-        const totalHits = initialStates[c] + incoming[c];
-
-        const neededStrikes = (k - (totalHits % k)) % k;
+        const neededStrikes = (k - states[c]) % k;
         totalStrikes += neededStrikes;
 
-        // Propagate the strikes to connected components
-        const neighbors = adj[c];
-        for (let i = 0; i < neighbors.length; i++) {
-            incoming[neighbors[i]] += neededStrikes;
+        // Propagate the strikes to connected components if needed
+        if (neededStrikes > 0) {
+            let edgeIdx = head[c];
+            while (edgeIdx !== -1) {
+                const neighbor = to[edgeIdx];
+                states[neighbor] = (states[neighbor] + neededStrikes) % k;
+                edgeIdx = next[edgeIdx];
+            }
         }
     }
 
